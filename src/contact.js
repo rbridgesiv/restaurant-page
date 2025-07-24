310a108b8f9123dc0bb1fa8193501896c4da1dfb
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-07-24 13:18:33 +0300

    change append structure of content

--- src/contact.js
+++ src/contact.js
@@ -5,21 +5,28 @@ export function loadContact() {
     header.textContent = 'Contact';
     content.appendChild(header);
 
-    const info = document.createElement('p');
-    info.textContent = `Contact Us
-
+    const address = document.createElement('p');
+    address.classList = 'address';
+    address.textContent = `
     Green Haven Vegetarian Bistro
     123 Greenway Blvd
-    Portland, OR 97204
+    Portland, OR 97204`
     
-    Phone: (503) 555-1234
-    Email: hello@greenhavenbistro.com
-    Website: www.greenhavenbistro.com
+    // const contact = document.createElement('p');
+    // contact.classList = 'contact';
+    // contact.textContent = `
+    // Phone: (503) 555-1234
+    // Email: hello@greenhavenbistro.com
+    // Website: www.greenhavenbistro.com`
     
-    Hours:
-    Monday–Friday: 11am – 9pm
-    Saturday–Sunday: 9am – 10pm`;
+    // const hours = document.createElement('p');
+    // hours.classList = 'hours';
+    // hours.textContent = `
+    // Hours:
+    // Monday–Friday: 11am – 9pm
+    // Saturday–Sunday: 9am – 10pm`;
 
-    content.appendChild(info);
-    
+    content.appendChild(address);
+    content.appendChild(contact);
+    content.appendChild(hours);
 }
