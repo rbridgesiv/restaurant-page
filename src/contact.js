13ee6cc193cdf3c7b2c11d9bcf5166210b59ff9f
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-07-24 13:34:17 +0300

    update contact info

--- src/contact.js
+++ src/contact.js
@@ -1,32 +1,40 @@
 export function loadContact() {
     const content = document.getElementById('content');
 
-    const header = document.createElement('h2');
-    header.textContent = 'Contact';
-    content.appendChild(header);
+    const hoursTitle = document.createElement('h3');
+    hoursTitle.textContent = 'Hours'
+    
+    const hours1 = document.createElement('p');
+    hours1.textContent = 'Monday–Friday: 11am – 10pm'
+    
+    const hours2 = document.createElement('p');
+    hours2.textContent = 'Saturday: 1pm – 11pm;'
+
+    const hours3 = document.createElement('p')
+    hours3.textContent = 'Sunday: 12pm - 6pm';
 
     const address = document.createElement('p');
     address.classList = 'address';
     address.textContent = `
-    Green Haven Vegetarian Bistro
     123 Greenway Blvd
     Portland, OR 97204`
     
-    // const contact = document.createElement('p');
-    // contact.classList = 'contact';
-    // contact.textContent = `
-    // Phone: (503) 555-1234
-    // Email: hello@greenhavenbistro.com
-    // Website: www.greenhavenbistro.com`
-    
-    // const hours = document.createElement('p');
-    // hours.classList = 'hours';
-    // hours.textContent = `
-    // Hours:
-    // Monday–Friday: 11am – 9pm
-    // Saturday–Sunday: 9am – 10pm`;
+    const phone = document.createElement('p');
+    phone.classList = 'phone';
+    phone.textContent = `
+    Phone: (503) 555-1234`
 
+    const email = document.createElement('p');
+    email.classList = 'email';
+    email.textContent = `Email: hello@vegehaven.com`;
+
+    
+    
+    content.appendChild(hoursTitle);
+    content.appendChild(hours1);
+    content.appendChild(hours2);
+    content.appendChild(hours3);
     content.appendChild(address);
-    content.appendChild(contact);
-    content.appendChild(hours);
+    content.appendChild(phone);
+    content.appendChild(email);
 }
