294539c59ce3ae94f883d8fbadd6a715cd6ef412
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-07-24 14:51:48 +0300

    remove text, add headline, change img size

--- src/page-content.js
+++ src/page-content.js
@@ -2,14 +2,17 @@ export function loadPageContent() {
     const content = document.getElementById('content');
 
     const header = document.createElement('h2');
-    header.textContent = 'Welcome to Vegetarian Haven!'
+    header.textContent = 'Welcome to Vegetarian Haven'
     content.appendChild(header);
+
+    const headline = document.createElement('h5');
+    headline.textContent = 'Where the garden meets the table.'
+    headline.classList = 'headline';
+    content.appendChild(headline);
 
     const img = document.createElement('img');
     img.src = "/images/restaurant_image.png";
     content.appendChild(img);
 
-    const text = document.createElement('p');
-    text.textContent = "Delight in the best of plant-based culinary excellence at our vegetarian restaurant. We serve an inspired menu full of flavorful, wholesome dishes made from fresh, local ingredients. From farm-fresh salads to hearty mains and decadent desserts, each plate is crafted to nourish and impress. Come experience a warm, inviting space where great taste meets mindful dining.";
-    content.appendChild(text);
+    
 };
